551435279e1c5a87ec1e36c7a1d78adcab3441ce
Will <avatec2312@gmail.com>
2023-01-14 17:13:09 -0500

    First base case

--- merge.js
+++ merge.js
@@ -1,2 +1,14 @@
 const exampleArray = [1, 6, 4, 3, 5];
 
+// On this side is the merge sort algorithm
+
+// Works with recursion
+
+function mergeSort (array) {
+
+    if(array < 0){
+        return
+    }
+    
+
+}
