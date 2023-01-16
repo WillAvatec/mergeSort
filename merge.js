4aa0d227e88b103f20d7f5ca2074efd39359399a
Will <avatec2312@gmail.com>
2023-01-15 22:59:18 -0500

    again trying with abdul method

--- merge.js
+++ merge.js
@@ -1,4 +1,4 @@
-const exampleArray = [1, 6, 4, 3, 5];
+const exampleArray = [5, 1, 3, 7];
 
 // On this side is the merge sort algorithm
 
@@ -6,9 +6,30 @@ const exampleArray = [1, 6, 4, 3, 5];
 
 function mergeSort (array) {
 
-    if(array < 0){
-        return
-    }
+    if (array.length === 1) return array;
     
+    //Just continue if the array items is more than 1
+    
+    const mid = Math.floor(array.length/2);
+
+    const leftArr = array.slice(0, mid);
+    const rightArr = array.slice(mid+1, array.length);
+
+    return merge(mergeSort(leftArr), mergeSort(rightArr))
+}
+
+const arraySorted = mergeSort(exampleArray);
+
+
+
+
+
+
+
+
+// This is where the magic borns
+
+function merge(arrayA, arrayB){
+
 
 }
